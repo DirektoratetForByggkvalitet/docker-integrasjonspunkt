260d32311ae94a1d6e7ba0fa7ad682c3b189f63a
Eirik Wulff <ew@dibk.no>
2023-04-25 12:00:02 +0200

    Ny metode for å installere s6

--- s6_install.sh
+++ s6_install.sh
@@ -3,20 +3,20 @@
 : "${TARGETARCH:=}"
 : "${S6_OVERLAY_VERSION:=3.1.4.2}"
 
-echo "Installerer S6-overlay, v${S6_OVERLAY_VERSION}"
+echo "Installerer S6-overlay, v${S6_OVERLAY_VERSION}, for ${TARGETARCH}"
 
 # Endrer på filnavnet dersom arkitekturen er arm64.
 if [ "$TARGETARCH" = "arm64" ]; then
-  ARCH="aarch64"
+  code="aarch64"
 else
-  ARCH=${TARGETARCH}
+  code=${TARGETARCH}
 fi
 
-declare -a files=("s6-overlay-noarch.tar.xz" "s6-overlay-symlinks-noarch.tar.xz" "s6-overlay-${ARCH}.tar.xz" "s6-overlay-symlinks-arch.tar.xz")
+declare -a files=("s6-overlay-noarch.tar.xz" "s6-overlay-symlinks-noarch.tar.xz" "s6-overlay-${code}.tar.xz" "s6-overlay-symlinks-arch.tar.xz")
 for file in "${files[@]}"
 do
     echo "Behandler '${file}'"
-    curl -Lso /tmp/${file} https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/${file}
+    curl -Lo /tmp/${file} https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/${file}
     tar -C / -Jxpf /tmp/${file}
 done
 
