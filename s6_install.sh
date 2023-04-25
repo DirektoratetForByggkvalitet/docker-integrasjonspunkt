8647fe6f87db08102bc61b71eee274aff187475c
Eirik Wulff <ew@dibk.no>
2023-04-25 11:26:32 +0200

    Ny versjon av s6_install.sh

--- s6_install.sh
+++ s6_install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-echo "Installerer S6-overlay"
-curl -Lso /tmp/s6-overlay-noarch.tar.xz  https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-noarch.tar.xz
-curl -Lso /tmp/s6-overlay-symlinks-noarch.tar.xz https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-symlinks-noarch.tar.xz
-curl -Lso /tmp/s6-overlay-${TARGETARCH}.tar.xz https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-${TARGETARCH}.tar.xz
-curl -Lso /tmp/s6-overlay-symlinks-arch.tar.xz https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-symlinks-arch.tar.xz
+: "${TARGETARCH:=}"
+: "${S6_OVERLAY_VERSION:=3.1.4.2}"
 
-tar -C / -Jxpf /tmp/s6-overlay-noarch.tar.xz
-tar -C / -Jxpf /tmp/s6-overlay-symlinks-noarch.tar.xz
-tar -C / -Jxpf /tmp/s6-overlay-${TARGETARCH}.tar.xz
-tar -C / -Jxpf /tmp/s6-overlay-symlinks-arch.tar.xz
+echo "Installerer S6-overlay"
+declare -a files=("s6-overlay-noarch.tar.xz" "s6-overlay-symlinks-noarch.tar.xz" "s6-overlay-${TARGETARCH}.tar.xz" "s6-overlay-symlinks-arch.tar.xz")
+for file in "${files[@]}"
+do
+    echo "Behandler '${file}'"
+    curl -Lso /tmp/${file} https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/${file}
+    tar -C / -Jxpf /tmp/${file}
+done
 
 echo "S6-overlay installert"
